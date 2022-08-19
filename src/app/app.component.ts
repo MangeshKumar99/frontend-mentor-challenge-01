164d6256cc779ab770ef229c1796fda467e208bd
Mangesh <mangesh2599@gmail.com>
2022-08-19 15:16:58 +0530

    landing page ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'landing-page-design';
+  changeIconFeature: boolean = false;
+  changeIconCompany: boolean = false;
+  isSideNavOpen: boolean = false;
+  isFeatureClicked: boolean = false;
+  isCompanyClicked: boolean = false;
+
+  openNav(){
+    this.isSideNavOpen = true;
+    setTimeout(()=>{
+      document.getElementById("mySidenav")!.style.width = "275px";
+    })
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  closeNav(){
+    document.getElementById("mySidenav")!.style.width = "0";
+    this.isSideNavOpen = false;
+    this.isFeatureClicked = false;
+    this.isCompanyClicked = false;
+    document.body.style.backgroundColor = "white";
+  }
+
 }
